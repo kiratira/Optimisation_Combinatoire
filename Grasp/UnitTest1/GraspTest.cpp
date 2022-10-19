d12656076e41863990bf3e4f7e3810cbb1718b23
titibs29 <thibaut.rizzoli@hotmail.be>
2022-10-19 14:34:10 +0200

    ajout de tests

--- Grasp/UnitTest1/GraspTest.cpp
+++ Grasp/UnitTest1/GraspTest.cpp
@@ -15,8 +15,8 @@ namespace GraspTest
 		{
 			Entree entree;
 
-			Assert::AreEqual((unsigned)0, entree.nbCouverture);
-			Assert::AreEqual((unsigned)0, entree.nbEmplacement);
+			Assert::AreEqual((unsigned char)0, entree.nbCouverture);
+			Assert::AreEqual((unsigned char)0, entree.nbEmplacement);
 			Assert::AreEqual(0.0f, entree.coutImpression);
 			Assert::AreEqual(0.0f, entree.coutFabrication);
 		}
@@ -25,7 +25,7 @@ namespace GraspTest
 		{
 			Solution sortie;
 
-			Assert::AreEqual(0U, sortie.nbPlaques);
+			Assert::AreEqual(0, (int)sortie.nbPlaques);
 			Assert::AreEqual(true, sortie.actif);
 			Assert::AreEqual(FLT_MAX, sortie.coutTotal);
 		}
@@ -39,22 +39,36 @@ namespace GraspTest
 			init(&sortie, &nbEmplacement);
 
 			Assert::AreEqual(0U, sortie.nbImpression[1]);
-			Assert::AreEqual(0U, sortie.agencement[3]);
+			Assert::AreEqual((unsigned char)0, sortie.agencement[3]);
 		}
+
 	};
 
-	TEST_CLASS(LectureTest) {
-
+	TEST_CLASS(lectureTest)
+	{
 	public:
 
-		TEST_METHOD(lecture1)
+		TEST_METHOD(isFile1Accessible)
 		{
 			Entree file1, expected;
 			expected.nbCouverture = 3;
 			expected.nbEmplacement = 4;
 			expected.coutImpression = 13.44;
 			expected.coutFabrication = 18676;
-			unsigned int nbDataset = 1;
+
+			std::ifstream myReadFile("../file1.in");
+			
+			Assert::AreEqual(myReadFile.is_open(), true);
+
+		}
+
+		TEST_METHOD(lectureFile1)
+		{
+			Entree file1, expected;
+			expected.nbCouverture = 3;
+			expected.nbEmplacement = 4;
+			expected.coutImpression = 13.44;
+			expected.coutFabrication = 18676;
 
 			lecture(&file1, "file1.in");
 
@@ -63,5 +77,83 @@ namespace GraspTest
 			Assert::AreEqual(expected.coutImpression, file1.coutImpression);
 			Assert::AreEqual(expected.coutFabrication, file1.coutFabrication);
 		}
+
+		TEST_METHOD(lectureEchec)
+		{
+			Entree file2, expected;
+			expected.nbCouverture = 3;
+			expected.nbEmplacement = 4;
+			expected.coutImpression = 13.44;
+			expected.coutFabrication = 18676;
+
+			lecture(&file2, "file2.in");
+
+			Assert::ExpectException<int>(lecture);
+		}
+
+
 	};
+
+	TEST_CLASS(checkPlaques) {
+
+	public:
+
+		TEST_METHOD(plaqueValide) {
+			std::vector<unsigned char> agencement = { 0, 1, 2, 3 };
+			unsigned char nbCouv = 4;
+
+			bool result = checkValiditePlaque(&agencement, &nbCouv);
+
+			Assert::AreEqual(result, true);
+		}
+
+		TEST_METHOD(plaqueNonValide) {
+			std::vector<unsigned char> agencement = { 0, 1, 2, 2 };
+			unsigned char nbCouv = 4;
+
+			bool result = checkValiditePlaque(&agencement, &nbCouv);
+
+			Assert::AreEqual(result, false);
+		}
+	};
+
+	TEST_CLASS(calculDuCout) {
+
+		TEST_METHOD(calculBonInt) {
+			std::vector<unsigned char> agencement = { 0, 1, 2, 3 };
+			std::vector<unsigned int> nbImpression = { 10000 };
+			unsigned short int nbPlaques = 1;
+			unsigned char nbEmplacement = 4;
+			float coutTotal = 0, coutImp = 1, coutFab = 100;
+
+			calculCout(&agencement,
+				&nbImpression,
+				&nbPlaques,
+				&nbEmplacement,
+				&coutTotal,
+				&coutImp,
+				&coutFab);
+
+			Assert::AreEqual(coutTotal, (float)10100);
+		}
+
+		TEST_METHOD(calculBonFloat) {
+			std::vector<unsigned char> agencement = { 0, 1, 2, 3 };
+			std::vector<unsigned int> nbImpression = { 10000 };
+			unsigned short int nbPlaques = 1;
+			unsigned char nbEmplacement = 4;
+			float coutTotal = 0, coutImp = 1.2 , coutFab = 100.3;
+
+			calculCout(&agencement,
+				&nbImpression,
+				&nbPlaques,
+				&nbEmplacement,
+				&coutTotal,
+				&coutImp,
+				&coutFab);
+
+			Assert::AreEqual(coutTotal, 12100.3f);
+		}
+	};
+
 }
